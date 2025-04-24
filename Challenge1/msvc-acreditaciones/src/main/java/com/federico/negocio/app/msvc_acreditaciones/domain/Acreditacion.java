fdb46e20fe922951455ef54373d753fcdddcdbca
Fedes23 <150834895+DserafiniGiraudo@users.noreply.github.com>
2025-04-24 10:24:45 -0300

    [FEATURE] persistencia acreditaciones en Mongo

--- Challenge1/msvc-acreditaciones/src/main/java/com/federico/negocio/app/msvc_acreditaciones/domain/Acreditacion.java
+++ Challenge1/msvc-acreditaciones/src/main/java/com/federico/negocio/app/msvc_acreditaciones/domain/Acreditacion.java
@@ -2,29 +2,28 @@ package com.federico.negocio.app.msvc_acreditaciones.domain;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
-@Table(name ="Acreditaciones")
+@Document(collection = "acreditacionesV2")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Acreditacion {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private double importe;
+    @Field("idPuntoVenta")
     private int identificadorPuntoVenta;
+    @Field("fechaRecepcion")
     private LocalDate fechaPedido;
     private String nombrePuntoventa;
 }
--- Challenge1/msvc-acreditaciones/src/main/java/com/federico/negocio/app/msvc_acreditaciones/repositories/AcreditacionRepository.java
+++ Challenge1/msvc-acreditaciones/src/main/java/com/federico/negocio/app/msvc_acreditaciones/repositories/AcreditacionRepository.java
@@ -1,9 +1,11 @@
 package com.federico.negocio.app.msvc_acreditaciones.repositories;
 
-import org.springframework.data.repository.ListCrudRepository;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.federico.negocio.app.msvc_acreditaciones.domain.Acreditacion;
 
-public interface AcreditacionRepository extends ListCrudRepository<Acreditacion, String> {
-
+public interface AcreditacionRepository extends MongoRepository<Acreditacion, String> {
+    List<Acreditacion> findByIdentificadorPuntoVenta(int identificadorPuntoVenta);
 }
